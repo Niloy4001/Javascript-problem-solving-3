7cd1d3113958bde7c578d866f3293e76fbba0228
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-09-08 17:05:48 +0600

    salary calculator

---
+++ task5.js
@@ -0,0 +1,45 @@
+
+
+///////////////////////////////////////////
+// ### Task -5: (Hard)
+// For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
+// ```
+//  const employees = [
+//             { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+//             { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+//             { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+//             { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+//         ];
+// ```
+
+
+
+const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+
+function calculateSalary(money) {
+
+    sum = 0;
+
+    for (const i of money) {
+
+        salaryOneMonth = i.starting + (i.experience * i.increment);
+
+        sum = sum + salaryOneMonth;
+
+    }
+
+    return sum;
+
+
+}
+
+
+const totalSalaryPerMonth = calculateSalary(employees);
+
+console.log(totalSalaryPerMonth);
