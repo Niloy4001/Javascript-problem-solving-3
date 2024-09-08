c9cb1b1e72aa46f92140b46425400a048098bbf7
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-09-08 14:18:29 +0600

    finding smallest word from array

---
+++ task2.js
@@ -0,0 +1,31 @@
+////////////////////////////////////////////
+
+// ### Task -2: 
+// Find the friend with the smallest name.
+// <br>
+// `const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];`
+
+// ---
+
+
+
+const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+
+function findSmallestName(words) {
+
+    let smallest = words[0];
+
+    for( const word of words){
+        if (word.length < smallest.length) {
+           
+            smallest = word;
+            
+        }
+    }
+    return smallest;
+    
+}
+
+const smallestName = findSmallestName(heights2);
+
+console.log(smallestName);
