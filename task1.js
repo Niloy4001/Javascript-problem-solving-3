665f59affca49e1d9fb040f2c119f66f1038f2f7
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-09-08 11:37:11 +0600

    finding the lowest value from an Array

---
+++ task1.js
@@ -0,0 +1,34 @@
+// ////////////////////////////////////////////////////
+// ### Task -1: 
+// Find the lowest number in the array below.
+// <br>
+// `const heights2 = [167, 190, 120, 165, 137];`
+
+// ---
+
+
+const heights2 = [167, 190, 120, 165, 137];
+
+function findLowestNumber(numbers) {
+    
+    let lowest = numbers[0]
+
+    for( const number of numbers){
+        
+        if (number < lowest) {
+            lowest = number;
+        }
+        
+    }
+
+    return lowest;
+    
+    
+    
+
+}
+
+const lowestNumber = findLowestNumber(heights2);
+console.log(lowestNumber);
+
+
